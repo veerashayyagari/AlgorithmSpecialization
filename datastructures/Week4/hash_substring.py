73987d5f0400038cb981edfa0b008de8a33a4639
Veeresh Ayyagari <veeru@conceptberry.com>
2017-01-27 04:47:22 -0800

    minor updates

--- datastructures/Week4/hash_substring.py
+++ datastructures/Week4/hash_substring.py
@@ -6,16 +6,22 @@ def read_input():
 def print_occurrences(output):
     print(' '.join(map(str, output)))
 
-def find_hash(text):
-	prime = 1000000007
-	x = 263
+def find_hash(text,prime,x):	
 	ans = 0
 	for c in reversed(text):
 		ans = ans * x + ord(c)
 		
 	return ans%prime
 
+def precompute_hashes(text,patternLength,prime,x):
+    textLength = len(text)
+    H = [0]*(textLength - patternLength + 1)
+    H[textLength - patternLength] = find_hash(text[textLength-patternLength-1:textLength-1],prime,x)
+    
+
 def get_occurrences(pattern, text):
+    prime = 1000000007
+    x = 263
     return [
         i 
         for i in range(len(text) - len(pattern) + 1) 
