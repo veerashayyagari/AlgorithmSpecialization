e500b391723a52c5f244418aae09964462c78a92
Veeresh Ayyagari <veeru@conceptberry.com>
2017-01-26 19:52:27 -0800

    Week3 and Week4

--- datastructures/Week3/job_queue.py
+++ datastructures/Week3/job_queue.py
@@ -1,33 +1,101 @@
 # python3
+class Node:
+  def __init__(self,value,priority):
+    self._value = value
+    self._priority = priority
+
+class BinaryMinHeap:
+  def __init__(self,maxSize):
+    self._data = [None]*maxSize
+    self._maxSize = maxSize
+    self._size = 0
+
+  def siftDown(self,i):
+    leftChild = 2*i + 1
+    rightChild = 2*i + 2
+
+    while leftChild < self._size:
+      minIdx = leftChild
+
+      if rightChild < self._size:
+        if ((self._data[rightChild]._priority < self._data[leftChild]._priority) or
+          (self._data[rightChild]._priority == self._data[leftChild]._priority and self._data[rightChild]._value < self._data[leftChild]._value)):
+
+          minIdx = rightChild
+
+      if ((self._data[minIdx]._priority < self._data[i]._priority) or
+        (self._data[minIdx]._priority == self._data[i]._priority and self._data[minIdx]._value < self._data[i]._value)):
+
+        self._data[minIdx],self._data[i] = self._data[i],self._data[minIdx]
+
+      i = minIdx
+      leftChild = 2*i + 1
+      rightChild = 2*i + 2
+
+
+  def siftUp(self,i):
+    parentIdx = (i - 1)//2
+
+    while parentIdx >= 0:
+
+      if ((self._data[parentIdx]._priority > self._data[i]._priority) or 
+        (self._data[parentIdx]._priority == self._data[i]._priority and self._data[parentIdx]._value > self._data[i]._value)):
+
+        self._data[parentIdx],self._data[i] = self._data[i],self._data[parentIdx]
+
+      i = parentIdx
+      parentIdx = (i - 1)//2
+
+
+  def insert(self,node):
+    if self._size < self._maxSize:
+      self._data[self._size] = node
+      self._size += 1
+      self.siftUp(self._size - 1)
+
+
+  def extractMin(self):
+    if self._size < 0 :
+      return None
+
+    result = self._data[0]
+    self._data[0],self._data[self._size-1] = self._data[self._size-1],self._data[0]
+    self._size -= 1
+    self.siftDown(0)
+    return result
+
+
+  def printOut(self):
+    for i in range(self._size):
+      print("Printing out.., Value : {0} and Priority : {1}".format(self._data[i]._value,self._data[i]._priority))
+
+
+
 
 class JobQueue:
     def read_data(self):
         self.num_workers, m = map(int, input().split())
         self.jobs = list(map(int, input().split()))
-        assert m == len(self.jobs)
-
-    def write_response(self):
-        for i in range(len(self.jobs)):
-          print(self.assigned_workers[i], self.start_times[i]) 
+        assert m == len(self.jobs) 
 
     def assign_jobs(self):
-        # TODO: replace this code with a faster algorithm.
-        self.assigned_workers = [None] * len(self.jobs)
-        self.start_times = [None] * len(self.jobs)
-        next_free_time = [0] * self.num_workers
-        for i in range(len(self.jobs)):
-          next_worker = 0
-          for j in range(self.num_workers):
-            if next_free_time[j] < next_free_time[next_worker]:
-              next_worker = j
-          self.assigned_workers[i] = next_worker
-          self.start_times[i] = next_free_time[next_worker]
-          next_free_time[next_worker] += self.jobs[i]
+        minHeap = BinaryMinHeap(self.num_workers)
+        m = len(self.jobs)
+        for i in range(m):
+          if i < self.num_workers:
+            node = Node(i,self.jobs[i])
+            minHeap.insert(node)
+            print(i,0)
+          else:
+            #minHeap.printOut()
+            node = minHeap.extractMin()
+            print(node._value,node._priority)
+            newNode = Node(node._value,node._priority+self.jobs[i])
+            minHeap.insert(newNode)
 
     def solve(self):
         self.read_data()
-        self.assign_jobs()
-        self.write_response()
+        self.assign_jobs()        
 
 if __name__ == '__main__':
     job_queue = JobQueue()
--- datastructures/Week3/merging_tables.py
+++ datastructures/Week3/merging_tables.py
@@ -2,30 +2,51 @@
 
 import sys
 
+
+class Table:
+    def __init__(self,id,rows):
+        self._id = id
+        self._rows = rows
+
+
+def getParent(table):
+    while table != parent[table]:
+        parent[table] = getParent(parent[table])
+        table = parent[table]
+
+    return table
+
+def merge(destination, source):
+    global maxRows
+    i_id = getParent(destination)
+    j_id = getParent(source)
+
+    if i_id == j_id:
+        return
+
+    if rank[i_id] > rank[j_id]:
+        parent[j_id] = i_id
+        tables[i_id]._rows += tables[j_id]._rows
+        if maxRows < tables[i_id]._rows:
+            maxRows = tables[i_id]._rows
+    else:
+        parent[i_id] = j_id
+        tables[j_id]._rows += tables[i_id]._rows
+        if maxRows < tables[j_id]._rows:
+            maxRows = tables[j_id]._rows
+
+        if rank[i_id] == rank[j_id]:
+            rank[j_id] += 1
+
+
 n, m = map(int, sys.stdin.readline().split())
 lines = list(map(int, sys.stdin.readline().split()))
 rank = [1] * n
 parent = list(range(0, n))
-ans = max(lines)
-
-def getParent(table):
-    # find parent and compress path
-    return parent[table]
-
-def merge(destination, source):
-    realDestination, realSource = getParent(destination), getParent(source)
-
-    if realDestination == realSource:
-        return False
-
-    # merge two components
-    # use union by rank heuristic 
-    # update ans with the new maximum table size
-    
-    return True
-
+tables = [Table(i,rowCount) for i,rowCount in enumerate(lines)]
+maxRows = max(lines)
 for i in range(m):
-    destination, source = map(int, sys.stdin.readline().split())
+    destination, source = map(int, sys.stdin.readline().split())    
     merge(destination - 1, source - 1)
-    print(ans)
+    print(maxRows)
     
