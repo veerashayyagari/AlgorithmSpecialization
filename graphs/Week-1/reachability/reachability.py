6e1de73462d10abbaea0022558d2357140526257
Veeresh Ayyagari <veeru@conceptberry.com>
2017-04-28 22:31:50 -0700

    reachability solution

--- graphs/Week-1/reachability/reachability.py
+++ graphs/Week-1/reachability/reachability.py
@@ -2,14 +2,29 @@
 
 import sys
 
+def explore(vertex,foundNode):
+    if foundNode is True or visited[vertex] is True:
+        return foundNode
+
+    visited[vertex] = True
+
+    for node in adj[vertex]:
+        if visited[node] is False:
+            if node == y:
+                return True
+            if(explore(node,foundNode)):
+                return True
+    return foundNode
+
 def reach(adj, x, y):
-    #write your code here
-    return 0
+    result = explore(x,False)
+    return 1 if result is True else 0
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = list(map(int, input.split()))
     n, m = data[0:2]
+    visited = [False]*n
     data = data[2:]
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
     x, y = data[2 * m:]
@@ -18,4 +33,5 @@ if __name__ == '__main__':
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
         adj[b - 1].append(a - 1)
+    #print(adj)
     print(reach(adj, x, y))
