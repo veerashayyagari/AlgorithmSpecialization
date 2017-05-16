c2c3808fa7a926c3a52dac43c50e65c11c81fa97
Veeresh Ayyagari <veeru@conceptberry.com>
2017-05-16 00:14:52 -0700

    shortest path solution

--- graphs/Week-4/shortest_paths/shortest_paths.py
+++ graphs/Week-4/shortest_paths/shortest_paths.py
@@ -1,12 +1,67 @@
 #Uses python3
 
 import sys
-import queue
+#import time
+from collections import deque
+
+
+def dfs(graph,cost,start,prev):
+    graph_length = len(graph)
+    q = deque([start])
+    visited = [False for _ in range(graph_length)]
+    relaxed_nodes = deque()
+
+    while len(q) > 0:
+        node_to_process = q.popleft()
+
+        if visited[node_to_process] is False:
+            visited[node_to_process] = True
+            for idx,node in enumerate(graph[node_to_process]):
+                q.append(node)
+                reachable[node] = 1
+                weighted_distance = distance[node_to_process] + cost[node_to_process][idx]
+                if weighted_distance < distance[node]:
+                    distance[node] = weighted_distance
+                    relaxed_nodes.append(node)   
+                    prev[node] = node_to_process                 
+    return relaxed_nodes
+
+
+def update_negative_cycle(graph,edge):
+    q = deque([edge])
+    shortest[edge] = 0
+    visited = [False for _ in range(len(graph))]
+
+    while len(q) > 0:
+        node_to_process = q.popleft()
+
+        if visited[node_to_process] is False:
+            visited[node_to_process] = True
+            for node in graph[node_to_process]:
+                q.append(node)
+                shortest[node] = 0
+
+
 
 
 def shortet_paths(adj, cost, s, distance, reachable, shortest):
-    #write your code here
-    pass
+    graph_length = len(adj)
+    distance[s] = 0
+    reachable[s] = 1  
+    prev = [None for _ in range(graph_length)]  
+
+    for i in range(1,graph_length+1):
+        negative_cycle = dfs(adj,cost,s,prev)
+        if len(negative_cycle) == 0:
+            break
+
+    while len(negative_cycle) > 0:
+        cycle_node = negative_cycle.popleft()
+        if shortest[cycle_node] != 0:
+            update_negative_cycle(adj,cycle_node)
+    
+
+
 
 
 if __name__ == '__main__':
@@ -23,9 +78,10 @@ if __name__ == '__main__':
         cost[a - 1].append(w)
     s = data[0]
     s -= 1
-    distance = [10**19] * n
+    distance = [float("inf")] * n
     reachable = [0] * n
     shortest = [1] * n
+    #t1 = time.time()
     shortet_paths(adj, cost, s, distance, reachable, shortest)
     for x in range(n):
         if reachable[x] == 0:
@@ -34,4 +90,6 @@ if __name__ == '__main__':
             print('-')
         else:
             print(distance[x])
+    #t2 = time.time()
+    #print((t2-t1)*1000)
 
